f0f43f044d2e62d32916214cc918607791d9e5ac
Tony <gm10285@gmail.com>
2022-11-06 22:07:49 +0800

    fixed

--- leetcode2011/solution_test.go
+++ leetcode2011/solution_test.go
@@ -1,12 +1,12 @@
 package leetcode2011
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func Test_finalValueAfterOperations(t *testing.T) {
 
 	res := finalValueAfterOperations([]string{"--X", "X++", "X++"})
-
-	if res != 1 {
-		t.Errorf("not match 1")
-	}
+	assert.Equal(t, 1, res)
 }
