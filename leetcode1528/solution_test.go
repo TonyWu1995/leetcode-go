ef318ba7884adc544274de20f0269f66d927b12b
Tony <gm10285@gmail.com>
2022-11-13 23:15:46 +0800

    impl

--- leetcode1528/solution_test.go
+++ leetcode1528/solution_test.go
@@ -1,14 +1,13 @@
 package leetcode1528
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func Test_restoreString(t *testing.T) {
 	s := "codeleet"
 	indices := []int{4, 5, 6, 7, 0, 2, 1, 3}
 
-	res := restoreString(s, indices)
-
-	if res != "leetcode" {
-		t.Errorf("no match")
-	}
+	assert.Equal(t, "leetcode", restoreString(s, indices))
 }
